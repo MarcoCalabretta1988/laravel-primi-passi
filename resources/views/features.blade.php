2fe3e95a83bd32d7c9471422ae9cddef59915d91
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-22 14:28:55 +0100

    Add other page and link

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First laravel</title>
+    <title>{{ $title }}</title>
     <!-- BOOTSTRAP -->
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -26,10 +26,10 @@
                             <a class="nav-link text-white" href="{{ route('features')}}">Features</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Pricing</a>
+                            <a class="nav-link text-white" href="{{ route('pricing')}}">Pricing</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled text-white">Disabled</a>
+                            <a class="nav-link text-white" href="{{ route('disabled')}}">Disabled</a>
                         </li>
                     </ul>
                 </div>
@@ -37,7 +37,7 @@
         </nav>
     </header>
     <main class="text-center my-5">
-        <h1>Hello world! {{ $message }}</h1>
+        <h1>{{ $title }} SECTION</h1>
     </main>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $message = 'This is my first app Laravel';
     return view('home', compact('message'));
-});
+})->name('index');
+
+Route::get('/features', function () {
+    $title = 'FEATURES';
+    return view('features', compact('title'));
+})->name('features');
+
+Route::get('/pricing', function () {
+    $title = 'PRICING';
+    return view('pricing', compact('title'));
+})->name('pricing');
+
+Route::get('/disabled', function () {
+    $title = 'DISABLED';
+    return view('disabled', compact('title'));
+})->name('disabled');
